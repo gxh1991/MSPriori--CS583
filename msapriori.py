288a8151ec17924a9e8dc5b878e1ae4d44715b01
Xinghao Gu <brian.gu1991@gmail.com>
2016-09-03 00:08:37 -0500

    read parameter

--- msapriori.py
+++ msapriori.py
@@ -2,12 +2,28 @@ import re
 
 parameterTextFileName = 'parameter-file.txt'
 dict = {}
+cannotBeTogether = []
+mustHave = []
+minsup = 0
 
-parameterText = open(parameterTextFileName)
-for line in parameterText:
-    matchMIS = re.search('\S*\((\d*)\)\D*(\d\.?\d*)', line)
-    matchSDC = re.search('SDC\D*(\d\.?\d*)', line)
-    if matchMIS:
-        dict[matchMIS.group(1)] = matchMIS.group(2)
-    if matchSDC:
-        print(matchSDC.group(1))
+
+def getParameterFromFile(Filename):
+    global dict, cannotBeTogether, mustHave, minsup
+    parameterText = open(Filename)
+    for line in parameterText:
+        matchMIS = re.search('\S*\((\d*)\)\D*(\d\.?\d*)', line)
+        matchSDC = re.search('SDC\D*(\d\.?\d*)', line)
+        matchTogether = re.search('cannot_be_together: {(.*)}', line)
+        matchMustHave = re.search('must-have: (.*)', line)
+        if matchMIS:
+            dict[matchMIS.group(1)] = matchMIS.group(2)
+        if matchSDC:
+            minsup = matchSDC.group(1)
+        if matchTogether:
+            together = matchTogether.group(1)
+            cannotBeTogether.append(together.split(','))
+        if matchMustHave:
+            have = matchMustHave.group(1)
+            for s in have.split('or'):
+                mustHave.append(s.strip())
+
